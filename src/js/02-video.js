f0e8c2e3f48c27fedb733e5e2b6ad89ce394a187
Tetiana Klimenko <ktlimenko@gmail.com>
2023-01-30 00:45:16 +0200

    Dz-08-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,70 @@
+
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Player(iframe);
 
-iframePlayer.on('play', function(data) {
-    console.log('played the video!');
-});
+const VIDEO_KEY = 'videoplayer-current-time';
+const onIframePlayer = function ({seconds}){
+    // console.log(data);
+    // console.log('played the video!');
+    try{
+   localStorage.setItem(VIDEO_KEY,JSON.stringify({seconds}));
+   }catch (err){
+    console.log(err);
+   };
+};
 
-iframePlayer.on('eventName', function(data) {
-    // data is an object containing properties specific to that event
-   console.log({data});
-});
+const throttleInfPlayer = throttle(onIframePlayer,1000)
 
-let callback = function() {};
+iframePlayer.on('timeupdate', throttleInfPlayer);
 
-iframePlayer.off('eventName', callback);
+const getCurrentTime = function (){
+    const getStory = localStorage.getItem(VIDEO_KEY) || 0;
+    const parseStory = JSON.parse(getStory);
+    console.log(parseStory.seconds);
+    return parseStory.seconds;  
+}
+
+ iframePlayer.setCurrentTime(getCurrentTime())
+
+// iframePlayer.on('play', function() {
+//     console.log('played the video!');
+//     console.log();
+// });
+// ======================================
+// iframePlayer.getCurrentTime(0.000).then(function(seconds) {
+//     // seconds = the current playback position
+//     console.log(seconds);
+// }).catch(function(error) {
+//     // an error occurred
+//     console.log(error);
+// });
+
+// iframePlayer.getPaused().then(function(paused) {
+//     // paused = whether or not the player is paused
+//     console.log(paused);
+
+// }).catch(function(error) {
+//     // an error occurred
+// });
+
+// iframePlayer.getPlayed().then(function(played) {
+//     // played = array values of the played video time ranges.
+//    console.log(played)
+    
+// }).catch(function(error) {
+//     // an error occurred
+// });
+
+// iframePlayer.getVideoEmbedCode().then(function(embedCode) {
+//     // embedCode = <iframe> embed code
+// }).catch(function(error) {
+//     // an error occurred
+// });
+// ========================================================
+
+
+
+
